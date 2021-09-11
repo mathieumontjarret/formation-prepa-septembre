4fb568f244ed5a601cd71d486a33632b4463dd6f
Mathieu Montjarret <mathieu.montjarret@icloud.com>
2021-09-11 18:01:57 +0200

    étape 14: router angular & lazy loading (fix page-not-found.module.ts)

--- src/app/page-not-found/page-not-found.module.ts
+++ src/app/page-not-found/page-not-found.module.ts
@@ -2,19 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PageNotFoundRoutingModule } from './page-not-found-routing.module';
-import { PageSignInComponent } from './pages/page-sign-in/page-sign-in.component';
-import { PageSignUpComponent } from './pages/page-sign-up/page-sign-up.component';
-import { PageResetPasswordComponent } from './pages/page-reset-password/page-reset-password.component';
-import { PageForgotPasswordComponent } from './pages/page-forgot-password/page-forgot-password.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 
 @NgModule({
   declarations: [
-    PageSignInComponent,
-    PageSignUpComponent,
-    PageResetPasswordComponent,
-    PageForgotPasswordComponent,
     PageNotFoundComponent
   ],
   imports: [
