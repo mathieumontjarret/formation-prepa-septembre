d23230e48dbcb31b4154be3bf98cd2b5ff772d7e
Mathieu Montjarret <mathieu.montjarret@icloud.com>
2021-09-12 13:47:11 +0200

    étape 21: observable subject et behaviorSubject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public title = 'formation-prepa-septembre';
+
+  private obs = new Observable((listXsubscribe) => {
+    listXsubscribe.next(Math.random());
+  });
+
+  private subj = new Subject();
+
+  private behave = new BehaviorSubject(0);
+
+  constructor(){
+    this.obs.subscribe((data) => console.log(data));
+    this.obs.subscribe((data) => console.log(data));
+
+    this.subj.subscribe((data) => console.log(data));
+    this.subj.next('test');
+    this.subj.next('test 2');
+    this.subj.subscribe((data) => console.log(data));
+    this.subj.next(Math.random());
+    this.subj.subscribe((data) => console.log(data));
+
+    this.behave.subscribe((data) => console.log(data));
+    this.behave.next(1);
+    this.behave.subscribe((data) => console.log(data));
+    this.behave.next(2);
+    this.behave.next(Math.random());
+    this.behave.subscribe((data) => console.log(data));
+    this.behave.subscribe((data) => console.log(data));
+
+
+  }
+
 }
